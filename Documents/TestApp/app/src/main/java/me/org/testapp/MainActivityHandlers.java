b50d7de72e160cdbebb0bad1ef0e1b0b8c6d059c
caoyielliot <caoyiee@outlook.com>
2016-04-09 15:00:28 -0700

    mini2

---
+++ Documents/TestApp/app/src/main/java/me/org/testapp/MainActivityHandlers.java
@@ -0,0 +1,7 @@
+package me.org.testapp;
+
+/**
+ * Created by caoyi on 16/3/19.
+ */
+public class MainActivityHandlers {
+}
---
+++ Documents/yc2_Mini2_Assignment2/app/src/main/java/me/org/yc2_mini2_assignment2/Exception/fixException.java
@@ -0,0 +1,7 @@
+package me.org.yc2_mini2_assignment2.Exception;
+
+/**
+ * Created by caoyi on 16/3/20.
+ */
+public class fixException {
+}
